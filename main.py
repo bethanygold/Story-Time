fd86c5fca96a667c0aaf90f5f49d07b76b606beb
bethanygold <61591794+bethanygold@users.noreply.github.com>
2023-02-24 13:14:13 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+silly_one = input("Type a silly word: ")
+name = input("Type a name: ")
+awkward = input("Type an awkward word: ")
+number = int(input("Type a number: "))
+silly_two = input("Type another silly word: ")
+funny_one = input("Type a funny word: ")
+silly_three = input("Type another silly word: ")
+funny_two = input("Type a funny word: ")
+silly_four = input("Type another silly word: ")
+
+print("")
+print("Dear H.O.D, ")
+print("")
+print(
+    f"{silly_one} {name} has been absent from {awkward} for the past {number} weeks and since has not being able to write you a {silly_two} letter, hence his {funny_one} has been revoked till further {silly_three}."
+)
+print("")
+print(f"Yours {funny_two},")
+print("")
+print(f"The {silly_four}")
+
+# Dear H.O.D, [silly] [name] has been absent from [akward] for the past [number] of weeks and since has not being able to write you a [silly] letter, hence his [funny word] has been revoked till further [silly]. Yours [funny word], The [silly]
